c5ec9d127f43919faec2b4d8d2548c577dc35e18
tony <antolisah@gmail.com>
2022-02-20 14:39:49 +0300

    added get and guess solution functions

--- src/lib.rs
+++ src/lib.rs
@@ -11,19 +11,35 @@ pub struct Contract {
 
 #[near_bindgen]
 impl Contract {
-    pub fn get_puzzle_number(&self) -> u8 {
-        PUZZLE_NUMBER
+    #[init]
+    pub fn new(solution: String) -> Self {
+        Self {
+            crossword_solution: solution,
+        }
     }
 
-    pub fn set_solution(&mut self, solution: String) {
-        self.crossword_solution = solution;
+    // pub fn get_puzzle_number(&self) -> u8 {
+    //     PUZZLE_NUMBER
+    // }
+
+    pub fn get_solution(&self) -> String {
+        self.crossword_solution.clone()
     }
 
-    pub fn guess_solution(&mut self, solution: String) {
-        if solution == self.crossword_solution {
-            env::log_str("You guessed right!")
+    // pub fn set_solution(&mut self, solution: String) {
+    //     self.crossword_solution = solution;
+    // }
+
+    pub fn guess_solution(&mut self, solution: String) -> bool {
+        let hashed_input = env::sha256(solution.as_bytes());
+        let hashed_input_hex = hex::encode(&hashed_input);
+
+        if hashed_input_hex == self.crossword_solution {
+            env::log_str("You guessed right!");
+            true
         } else {
-            env::log_str("You guessed wrong. Try again!")
+            env::log_str("Try again.");
+            false
         }
     }
 }
@@ -50,7 +66,6 @@ mod tests {
         builder
     }
 
-    // TESTS HERE
     #[test]
     fn debug_get_hash() {
         // Basic set up for a unit test
@@ -61,5 +76,29 @@ mod tests {
         let debug_hash_bytes = env::sha256(debug_solution.as_bytes());
         let debug_hash_string = hex::encode(debug_hash_bytes);
         println!("Let's debug: {:?}", debug_hash_string);
+    }
+
+    #[test]
+    fn check_guess_solution() {
+        // Get Alice as an account ID
+        let alice = AccountId::new_unchecked("alice.testnet".to_string());
+        // Set up the testing context and unit test environment
+        let context = get_context(alice);
+        testing_env!(context.build());
+
+        // Set up contract object and call the new method
+        let mut contract = Contract::new(
+            "69c2feb084439956193f4c21936025f14a5a5a78979d67ae34762e18a7206a0f".to_string(),
+        );
+        let mut guess_result = contract.guess_solution("wrong answer here".to_string());
+        assert!(!guess_result, "Expected a failure from the wrong guess");
+        assert_eq!(get_logs(), ["Try again."], "Expected a failure log.");
+        guess_result = contract.guess_solution("near nomicon ref finance".to_string());
+        assert!(guess_result, "Expected the correct answer to return true.");
+        assert_eq!(
+            get_logs(),
+            ["Try again.", "You guessed right!"],
+            "Expected a successful log after the previous failed log."
+        );
     }
 }
